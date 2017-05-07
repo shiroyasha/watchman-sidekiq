f0026be4018fd2da7fa3165e10226200392b8775
Igor Šarčević <igor@renderedtext.com>
2017-05-07 14:18:18 +0200

    Remove default failing spec

--- spec/watchman/sidekiq_spec.rb
+++ spec/watchman/sidekiq_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Watchman::Sidekiq do
   it "has a version number" do
     expect(Watchman::Sidekiq::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
